8d36600b6011cc3a763faf497444089bf9b1fb79
David Green <dgreen99@gmail.com>
2010-07-28 22:36:01 -0700

    copyright

--- greensopinion.restexample.test/src/greensopinion/restexample/test/jpa/TestEntityManagerFactory.java
+++ greensopinion.restexample.test/src/greensopinion/restexample/test/jpa/TestEntityManagerFactory.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 David Green.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Green - initial API and implementation
+ *******************************************************************************/
 package greensopinion.restexample.test.jpa;
 
 import java.util.Map;
